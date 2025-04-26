6643fc455ad378ae712134ed4b7f1c7b74e4fb73
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-04-26 21:38:11 +0300

    react icons and the title is done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,16 @@
 import React, { useRef, useState } from "react";
+import Button from "./Button";
 
 const Hero = () => {
-  const { currentIndex, setCurrentIndex } = useState(0);
-  const { hasClicked, setHasClicked } = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
   const [loading, setLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
   const totalVideos = 4;
 
-  const nextVideoRef = useRef(null);
+  // presist between
+  const nextVideoRef = useRef(null); // reference to the next video
 
   // 0 % 4 = 0 + 1 => 1
   // 1 % 4 = 1 + 1 => 2
@@ -19,7 +21,7 @@ const Hero = () => {
 
   const handleMiniVdClick = () => {
     setHasClicked(true);
-    setCurrentIndex((prevState) => prevState + 1);
+    setCurrentIndex(upcomingVideoIndex);
   };
 
   const handleVideoLoad = () => {
@@ -27,6 +29,7 @@ const Hero = () => {
   };
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
       <div
@@ -34,14 +37,14 @@ const Hero = () => {
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div>
-          <div className="mask-clip-path absolute absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
             <div
               onClick={handleMiniVdClick}
-              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in-out hover:scale-100 hover:opacity-100"
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
             >
               <video
                 ref={nextVideoRef}
-                src={getVideoSrc(currentIndex + 1)}
+                src={getVideoSrc(upcomingVideoIndex)}
                 loop
                 muted
                 id="current-video"
@@ -49,6 +52,40 @@ const Hero = () => {
                 onLoadedData={handleVideoLoad}
               />
             </div>
+          </div>
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            // autoPlay
+            muted
+            loop
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br />
+              Unleash the Play Economy
+            </p>
+            <Button id="watch-trailer" title="Watch Trailer" leftIcon={}/>
           </div>
         </div>
       </div>
